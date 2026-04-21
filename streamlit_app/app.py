dbc6f260831af214d933e497e97f4e89adbc0100
Karthik Venkata <karthikdattatraya@gmail.com>
2026-04-21 09:03:30 +0530

    Fix Streamlit app and requirements for deployment

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,11 +3,11 @@ from model_helper import predict
 
 
 st.title("Vehicle Damage Detection")
-uploaded_file=st.file_uploader("Upload the file",type=["jpg","png"])
+uploaded_file=st.file_uploader("Upload the file",type=["jpg","png"]) #file uploader ie taking image as an input
 if uploaded_file:
     image_path="temp_file.jpg"
-    with open(image_path,"wb") as f:
-        f.write(uploaded_file.getbuffer())
-        st.image(uploaded_file,caption="uploaded File",width="content")
+    with open(image_path,"wb") as f: # storing the binary data of an image in another temporary file ,w means wirte b means binary
+        f.write(uploaded_file.getbuffer()) #gives the binary bytes of the uploaded  image
+        st.image(uploaded_file,caption="uploaded File",width="content") #displayin the file
         prediction=predict(image_path)
         st.info(f"Predicted Class:{prediction}")
